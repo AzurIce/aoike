ed3ebe01413cf1387d7db6de9057877ba1f7e669
Azur冰弦 <973562770@qq.com>
2023-03-22 14:54:57 +0800

    feat: style for blockquotes and hrs

--- aoike/commands/test_build.py
+++ aoike/commands/test_build.py
@@ -4,7 +4,9 @@ import unittest
 
 class TestBuild(unittest.TestCase):
     def test_build(self):
-        os.chdir(r'D:\_Dev\aoike')
+        # os.chdir(r'D:\_Dev\aoike')
+        os.chdir(r'H:\Projects\aoike')
 
         import build
-        build.build(src_dir=r'D:\_Dev\azurice.github.io')
+        # build.build(src_dir=r'D:\_Dev\azurice.github.io')
+        build.build(src_dir=r'F:\azurice.github.io')
