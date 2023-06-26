5e514c0c81df6dba441212e2a7d985663776a196
Azur冰弦 <973562770@qq.com>
2023-06-26 17:36:37 +0800

    added basic dev server info display

--- mod.ts
+++ mod.ts
@@ -1,12 +1,15 @@
 import { createServer, mergeConfig as mergeViteConfig } from 'npm:vite'
 import vue from 'npm:@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { options, blue, bold, cyan, dim, gray, green, underline, yellow } from 'npm:kolorist'
 
 
 import viteConfig from './vue-project/vite.config.ts'
 
 // const root = "./vue-project"
 console.log("Hello")
+// Deno.env.set("NODE_ENV", "development")
+// console.log(Deno.env.get("NODE_ENV"))
 
 let server = await createServer({
     configFile: false, // 禁用 Vite 配置文件自动解析
@@ -22,3 +25,13 @@ let server = await createServer({
 })
 // let server = await createServer(viteConfig)
 await server.listen()
+printInfo()
+
+function printInfo() {
+    options.enabled = true;
+    console.log()
+    console.log(`  ${bold('Aoike')}`)
+    console.log()
+    console.log(`${dim('  Preview   ')} > ${cyan(`http://localhost:127.0.0.1/`)}`)
+    console.log()
+}
