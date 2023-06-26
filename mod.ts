7127cdfdccf538c8cd45e78a6bb4d165569df612
Azur冰弦 <973562770@qq.com>
2023-06-26 12:25:18 +0800

    It works🎉

--- mod.ts
+++ mod.ts
@@ -1,1 +1,24 @@
+import { createServer, mergeConfig as mergeViteConfig } from 'npm:vite'
+import vue from 'npm:@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+
+import viteConfig from './vue-project/vite.config.ts'
+
+// const root = "./vue-project"
 console.log("Hello")
+
+let server = await createServer({
+    configFile: false, // 禁用 Vite 配置文件自动解析
+    root: "./vue-project/",
+    plugins: [
+        vue()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./vue-project/src', import.meta.url))
+      }
+    }
+})
+// let server = await createServer(viteConfig)
+await server.listen()
